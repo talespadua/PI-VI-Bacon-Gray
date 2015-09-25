313af391217f52fdbb3c478826210e9c9857478d
Tales Carlos de Pádua <talescpadua@gmail.com>
2015-09-24 21:38:40 -0300

    Menu sucesso, bug na volta do mainloop

--- menu.c
+++ menu.c
@@ -7,28 +7,48 @@
 
 void menu()
 {
-    bool menu_ativo = true;
+    bool menu_ativo = true, entra = false;
     typedef enum{UP, DOWN, LEFT, RIGHT};
     bool teclas[] = {false, false, false, false};
 
     //TTF_Font *gFont = NULL;
 
-    SDL_Rect r = {10,10,32,32};
-    SDL_Rect rcSprite = {0,0, 64,205};
+    SDL_Renderer *screenRenderer = NULL;
+
+    SDL_Rect ini_ret = {350,300,163,56};
+    SDL_Rect sair_ret = {350,380, 107,56};
+    SDL_Rect name_ret = {0,0, 800,200};
+    SDL_Rect select = {350,300,163,56};
+
     SDL_Event eventos;
-
+    screenRenderer = SDL_CreateRenderer( game.window, -1, SDL_RENDERER_ACCELERATED );
     //Imagens do Menu
-    SDL_Surface *iniciar, *sair;
+    SDL_Surface *iniciar, *sair, *name;
+    SDL_Texture *iniciar_textura, *sair_textura, *name_textura;
 
-    imagem = SDL_LoadBMP("iniciar.bmp");
 
-    if( imagem == NULL )
+    iniciar = SDL_LoadBMP("iniciar.bmp");
+    iniciar_textura = SDL_CreateTextureFromSurface( screenRenderer, iniciar );
+    SDL_FreeSurface(iniciar);
+
+    sair = SDL_LoadBMP("sair.bmp");
+    sair_textura = SDL_CreateTextureFromSurface( screenRenderer, sair );
+    SDL_FreeSurface(sair);
+
+    name = SDL_LoadBMP("name.bmp");
+    name_textura = SDL_CreateTextureFromSurface( screenRenderer, name );
+    SDL_FreeSurface(name);
+
+
+
+    if( name == NULL )
     {
         printf("Imagem nao pode ser carregada!  SDL_Error: %s\n", SDL_GetError() );
         SDL_Quit();
         return 2;
     }
-    SDL_SetColorKey(imagem, SDL_TRUE, SDL_MapRGB(imagem->format, 0, 255, 255));
+
+    //SDL_SetColorKey(imagem, SDL_TRUE, SDL_MapRGB(imagem->format, 0, 255, 255));
     /*FIM DA INICIALIZACAO*/
 
     while(menu_ativo)
@@ -43,6 +63,9 @@ void menu()
                 if(eventos.key.keysym.sym == SDLK_UP){
                       teclas[UP] = true;
                 }
+                if(eventos.key.keysym.sym == SDLK_RETURN){
+                      entra = true;
+                }
             }
             if(eventos.type == SDL_KEYUP)
             {
@@ -52,6 +75,9 @@ void menu()
                 if(eventos.key.keysym.sym == SDLK_UP){
                     teclas[UP] = false;
                 }
+                if(eventos.key.keysym.sym == SDLK_RETURN){
+                      entra = false;
+                }
             }
             if(eventos.type == SDL_QUIT){
                 menu_ativo = 0;
@@ -59,28 +85,32 @@ void menu()
         }
 
         if(teclas[UP]){
-            mainloop();
+            //mainloop();
+            select.y = ini_ret.y;
         }
         if(teclas[DOWN]){
-            r.y+=1;
+            select.y = sair_ret.y;
         }
-        if(teclas[LEFT]){
-            r.x-=1;
-            rcSprite.x -=64;
-            if(rcSprite.x <= 0)
-                rcSprite.x = 192;
+        if(entra){
+            if(select.y == ini_ret.y){
+                mainloop();
+            }
+            if(select.y == sair_ret.y){
+                exit(0);
+            }
         }
-        if(teclas[RIGHT]){
-            r.x+=1;
-            rcSprite.x+=64;
-            if(rcSprite.x >= 256)
-                rcSprite.x = 0;
-        }
+        SDL_SetRenderDrawColor( screenRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+        SDL_RenderClear(screenRenderer);
 
-        //printf("\n");
-        SDL_FillRect(game.screenSurface, NULL, 0xffffff);
         //SDL_FillRect(screenSurface, &r, 0xFF0000);
-        SDL_UpdateWindowSurface(game.window);
+        SDL_RenderCopy(screenRenderer, iniciar_textura, NULL, &ini_ret/*&rectDestino*/);
+        SDL_RenderCopy(screenRenderer, sair_textura, NULL, &sair_ret/*&rectDestino*/);
+        SDL_RenderCopy(screenRenderer, name_textura, NULL, &name_ret/*&rectDestino*/);
+
+        SDL_SetRenderDrawColor( screenRenderer, 0xFF, 0x00, 0x00, 0xFF );
+        SDL_RenderDrawRect( screenRenderer, &select );
+
+        SDL_RenderPresent(screenRenderer);
         SDL_Delay(50);
     }
     //Destroi window
