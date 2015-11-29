50a3ab5ea31910c217825801db5c5627c2ea83bf
Vinicius de Carvalho <carvalho.v@outlook.com>
2015-11-29 17:07:54 -0200

    Changes made to the client to respect the application protocol

--- client/client/main.c
+++ client/client/main.c
@@ -1,40 +1,41 @@
 #include <stdio.h>
 #include <winsock.h>
+#include <ctype.h>
 
-#define ADDRESS "127.0.0.1"
+#include "utils.h"
+
 #define PORTA   5193
 
 int num_jogador;
 char address[16];
 
-void inicio(int *socket_cliente){
+int isPlayerOne = 0;
+int opponentAssit;
+
+void inicio(int *socket_cliente) {
     char buffer[627];
     int aux = 0;
 
-    printf("\nAguardando OK do servidor...");
+    printf("Aguardando instruções do servidor...\n");
     recv(socket_cliente, buffer, 100, 0);
-    printf("\nRecebido: |%s|", buffer);
+    printf("Recebido: |%s|\n", buffer);
 
-    if(buffer[2] == 'm'){
-        num_jogador = 0;
-        printf("\nJogador pode escolher mapa");
-        printf("\n[num_ajudante][num_mapa]\n");
-    } else {
-        num_jogador = 1;
-        printf("\nJogador não pode escolher mapa");
-        printf("\n[num_ajudante]\n");
+    if (buffer[0] == ID_CHOOSE_PLAYER_1) {
+        isPlayerOne = 1;
     }
 
+    printf("Choose your destiny:\n");
     scanf("%s", buffer);
+    buffer[2] = isPlayerOne ? buffer[1] : '0';
+    buffer[1] = buffer[0];
+    buffer[0] = ID_PLAYER_READY;
+
     send(socket_cliente, buffer, strlen(buffer)+1, 0);
 
     recv(socket_cliente, buffer, 100, 0);
     printf("\nRecebido: |%s|", buffer);
 
-    if(!strcmp(buffer, "ini"))
-        printf("\n\nIniciando jogo...\n\n");
-
-    while(strcmp(buffer, "end")){
+    while (strcmp(buffer, "end")) {
         recv(socket_cliente, buffer, 627, 0);
         printf("\n\nRecebido:\n%s", buffer);
     }
@@ -44,46 +45,48 @@ void jogo(int *socket_cliente){
 
 }
 
-void config(){
-    FILE *arquivo;
+void config() {
+    FILE *configFile;
     int flag = 0;
     int i = 0;
     char data;
     char aux;
 
-    arquivo = fopen("config.txt", "r");
-    while((data = fgetc(arquivo)) != EOF){
-        aux = (char) data;
-        if(data == '\n'){
-            flag = 1;
-            address[i] = '\0';
-            break;
-        } else {
-            address[i++] = data;
-        }
+    configFile = fopen("config.txt", "r");
+    if (!configFile) {
+        printf("Erro ao abrir arquivo de configuracao!\n");
+        exit(1);
     }
-    fclose(arquivo);
+
+    while ((data = fgetc(configFile)) != EOF) {
+        aux = (char) data;
+
+        if (isspace(data)) {
+            break;
+        }
+
+        address[i++] = data;
+    }
+
+    address[i] = '\0';
+    fclose(configFile);
 }
 
-int main( void )
-{
+int main(void) {
     int desc_socket;
     struct sockaddr_in endereco;
-
-    char buffer[512];
-    int sair = 0;
 
     config();
 
     printf( "Iniciando WinSock API...\n" );
     WSADATA wsaData;
 
-    if(WSAStartup(MAKEWORD(1, 1), &wsaData)!= 0){
+    if (WSAStartup(MAKEWORD(1, 1), &wsaData)!= 0) {
         printf("Cliente: WINSOCK.DLL nao encontrado \n" );
         exit( 1 );
     }
 
-    if((desc_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    if ((desc_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         printf("Cliente: errro na criacao do socket \n");
         exit(1);
     }
@@ -92,11 +95,10 @@ int main( void )
     endereco.sin_port = htons(PORTA);
     endereco.sin_addr.s_addr = inet_addr(address);
 
-    if(connect(desc_socket, (struct sockaddr *) &endereco, sizeof(endereco)) < 0) {
+    if (connect(desc_socket, (struct sockaddr *) &endereco, sizeof(endereco)) < 0) {
         printf("Cliente: erro na conexao \n");
         exit(1);
     }
-
 
     inicio(desc_socket);
     jogo(desc_socket);
